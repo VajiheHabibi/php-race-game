36eded484d215d22425edf76cfcbf040470761f8
Vajihe <vajihe.habibi@gmail.com>
2024-02-15 20:52:07 +0330

    read data from json file

--- cli/race-cli.php
+++ cli/race-cli.php
@@ -1,3 +1,13 @@
 <?php
 
+use App\Helpers\JsonReader;
+
 require __DIR__ . '/../vendor/autoload.php';
+
+try {
+    $jsonReader = new JsonReader();
+    $vehicles = $jsonReader->readJsonFile(__DIR__ . '/../data/vehicles.json');
+        var_dump($vehicles);
+} catch (\Exception $e) {
+    echo "An error occurred: " . $e->getMessage() . "\n";
+}
