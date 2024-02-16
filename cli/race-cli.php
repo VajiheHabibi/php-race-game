7ad3f4ae7fcdd30eb2a4fcfd6a7ee04d09d5b70a
Vajihe <vajihe.habibi@gmail.com>
2024-02-16 21:40:50 +0330

    show vehicle list

--- cli/race-cli.php
+++ cli/race-cli.php
@@ -1,13 +1,27 @@
 <?php
 
+require __DIR__ . '/../vendor/autoload.php';
 use App\Helpers\JsonReader;
 
-require __DIR__ . '/../vendor/autoload.php';
-
-try {
+//try {
     $jsonReader = new JsonReader();
     $vehicles = $jsonReader->readJsonFile(__DIR__ . '/../data/vehicles.json');
-        var_dump($vehicles);
-} catch (\Exception $e) {
+
+
+
+    // Display the list of vehicles
+    $table = new cli\Table;
+    $table->setHeaders(['Index', 'Vehicle Name', 'Max Speed', 'Unit']);
+
+    foreach ($vehicles as $index => $vehicle) {
+        $table->addRow([$index, $vehicle['name'], $vehicle['maxSpeed'], $vehicle['unit']]);
+    }
+    $table->display();
+
+    //select index
+    //convert klm and use same unit
+    //run match
+
+/*} catch (\Exception $e) {
     echo "An error occurred: " . $e->getMessage() . "\n";
-}
+}*/
